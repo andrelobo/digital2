43c1ae99e1b7b07c5db8f68d49340630cd126e40
André Lobo <loboandre@hotmail.com>
2024-10-16 11:01:23 -0400

    feat:google analytcs

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
-import Layout from "@/components/common/Layout"
-import "@/styles/main.scss"
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Layout from "@/components/common/Layout";
+import "@/styles/main.scss";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-7GHFKT2MP9', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Remover o listener quando o componente for desmontado
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
